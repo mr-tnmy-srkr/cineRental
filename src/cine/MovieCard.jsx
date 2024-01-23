8d40268f2a59d89e797c84a721f899dbae9b304b
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-01-23 19:02:21 +0530

    modal functionality added

--- src/cine/MovieCard.jsx
+++ src/cine/MovieCard.jsx
@@ -1,16 +1,16 @@
-// import { useState, useContext } from "react";
+import { useState,  } from "react";
 import { getImgUrl } from "../utils/cine-utility";
-// import Rating from "./Rating";
-// import MovieDetailsModal from "./MovieDetailsModal";
+import Rating from "./Rating";
+import MovieDetailsModal from "./MovieDetailsModal";
 // import { MovieContext } from "../context";
 // import { toast } from 'react-toastify';
 
 export default function MovieCard({ movie }) {
-  /*  const [showModal, setShowModal] = useState(false);
+   const [showModal, setShowModal] = useState(false);
     const [selectedMovie, setSelectedMovie] = useState(null);
-    const { state, dispatch } = useContext(MovieContext);
+    // const { state, dispatch } = useContext(MovieContext);
 
-    function hadleAddToCart(event, movie) {
+  /*   function hadleAddToCart(event, movie) {
         event.stopPropagation();
 
         const found = state.cartData.find((item) => {
@@ -33,8 +33,8 @@ export default function MovieCard({ movie }) {
             });
         }
     }
-
-    function hadleMovieSelection(movie) {
+ */
+    function handleMovieSelection(movie) {
         setSelectedMovie(movie);
         setShowModal(true);
     }
@@ -43,20 +43,20 @@ export default function MovieCard({ movie }) {
         setSelectedMovie(null);
         setShowModal(false);
     }
- */
+
   return (
     <>
-      {/* {showModal && (
+     {showModal && (
                 <MovieDetailsModal
                     movie={selectedMovie}
                     onClose={handleModalClose}
-                    onCartAdd={hadleAddToCart}
+                    // onCartAdd={hadleAddToCart}
                 />
-            )} */}
+            )} 
       <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
         <a
           href="#"
-          // onClick={() => hadleMovieSelection(movie)}
+          onClick={() => handleMovieSelection(movie)}
         >
           <img
             className="w-full object-cover"
@@ -67,7 +67,7 @@ export default function MovieCard({ movie }) {
             <h3 className="text-xl mb-1">{movie.title}</h3>
             <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
             <div className="flex items-center space-x-1 mb-5">
-              {/* <Rating value={movie.rating} /> */}
+              <Rating value={movie.rating} />
             </div>
             <button
               className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
